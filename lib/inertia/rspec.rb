43800a67308f6382cac72c928616e47479a5bf1b
Roman Samoilov <2270393+rsamoilov@users.noreply.github.com>
2026-07-15 10:25:55 +0100

    Add RSpec helpers

---
+++ lib/inertia/rspec.rb
@@ -0,0 +1,128 @@
+# frozen_string_literal: true
+
+require "rage/rspec"
+
+module Inertia
+  module RSpec
+    # Wraps an Inertia response for test assertions.
+    #
+    # @example Accessing response data
+    #   expect(inertia.component).to eq("Users/Show")
+    #   expect(inertia.props[:user]).to include({ name: "John" })
+    class TestResponse
+      # @param data [Hash] the raw Inertia response data
+      def initialize(data)
+        @data = data
+      end
+
+      # Returns the props with symbolized keys.
+      #
+      # @return [Hash{Symbol => Object}] the props hash
+      def props
+        sybmolize_keys(@data[:props])
+      end
+
+      # Returns the component name.
+      #
+      # @return [String] the Inertia component name
+      def component
+        @data[:component]
+      end
+
+      # Returns the deferred props configuration.
+      #
+      # @return [Hash] the deferred props, or an empty hash if none
+      def deferred_props
+        @data[:deferredProps] || {}
+      end
+
+      private
+
+      def sybmolize_keys(data)
+        data.each_with_object({}).each do |(k, v), memo|
+          transformed_value = if v.is_a?(Hash)
+            sybmolize_keys(v)
+          elsif v.is_a?(Array)
+            v.map { |el| el.is_a?(Hash) ? sybmolize_keys(el) : el }
+          else
+            v
+          end
+
+          memo[k.to_sym] = transformed_value
+        end
+      end
+    end
+
+    module TestHelpers
+      # Returns the Inertia response from the last request.
+      #
+      # @return [TestResponse, nil] the wrapped Inertia response, or nil if the last
+      #   request was not an Inertia response
+      #
+      # @example
+      #   get "/users/1"
+      #   expect(inertia.component).to eq("Users/Show")
+      #   expect(inertia.props[:user]).to include(name: "John")
+      def inertia
+        inertia_response = Fiber.current.instance_variable_get(:@__inertia_current_response)
+        return unless inertia_response
+
+        TestResponse.new(inertia_response)
+      end
+    end
+
+    module RequestHelpers
+      # Performs a GET request with optional Inertia partial reload headers.
+      #
+      # @param inertia [Hash] partial reload options
+      # @option inertia [String, Symbol, Array<String, Symbol>] :only request only these props
+      # @option inertia [String, Symbol, Array<String, Symbol>] :except request all props except these
+      # @param headers [Hash] additional request headers
+      # @raise [ArgumentError] if unknown options are passed in the inertia hash
+      #
+      # @example Request only specific props
+      #   get "/users/1", inertia: { only: [:user, :permissions] }
+      #
+      # @example Request all props except some
+      #   get "/users/1", inertia: { except: :audit_log }
+      def get(*, inertia: {}, headers: {}, **)
+        if inertia.except(:only, :except).any?
+          raise ArgumentError, "Unknown :inertia option. Supported values are :only and :except"
+        end
+
+        if inertia.any?
+          headers = headers.merge({
+            "X-Inertia" => "true",
+            "X-Inertia-Partial-Component" => self.inertia&.component || double(:== => true)
+          })
+
+          if only = inertia[:only]
+            headers["X-Inertia-Partial-Data"] = Array(only).join(",")
+          elsif except = inertia[:except]
+            headers["X-Inertia-Partial-Except"] = Array(except).join(",")
+          end
+        end
+
+        super(*, headers:, **)
+      end
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include(Inertia::RSpec::TestHelpers, type: :request)
+  config.prepend(Inertia::RSpec::RequestHelpers, type: :request)
+
+  config.before(:each) do
+    Fiber.current.instance_variable_set(:@__inertia_current_response, nil)
+  end
+
+  config.before(:each) do
+    test_fiber = Fiber.current
+
+    allow_any_instance_of(Inertia::ProtocolBuilder).to receive(:call).and_wrap_original do |m, *args, **kwargs|
+      data = m.call(*args, **kwargs)
+      test_fiber.instance_variable_set(:@__inertia_current_response, data)
+    end
+  end
+end
---
+++ spec/inertia/rspec/rspec_spec.rb
@@ -0,0 +1,157 @@
+# frozen_string_literal: true
+
+require "rage/all"
+
+module RspecSpecHelpers
+  class TestController < RageController::API
+    def index
+      render inertia: "Users/Index", props: {
+        "users" => [{ "id" => 1, "name" => "John" }, { "id" => 2, "name" => "Jane" }],
+        "meta" => { "total" => 2 }
+      }
+    end
+
+    def show
+      render inertia: "Users/Show", props: {
+        user: { id: 1, name: "John", email: "john@example.com" },
+        permissions: Inertia.optional { %w[read write] },
+        activity: Inertia.deferred { %w[login logout] }
+      }
+    end
+
+    def plain
+      render plain: "Hello, World!"
+    end
+
+    def json_response
+      render json: { message: "Hello" }
+    end
+  end
+
+  Rage.routes.draw do
+    root to: "rspec_spec_helpers/test#index"
+    get "/users/:id", to: "rspec_spec_helpers/test#show"
+    get "/plain", to: "rspec_spec_helpers/test#plain"
+    get "/json", to: "rspec_spec_helpers/test#json_response"
+  end
+end
+
+RSpec.describe "Inertia::RSpec", type: :request do
+  before :context do
+    Rage.instance_variable_set(:@root, Pathname.new(__dir__).expand_path)
+    Rage.instance_variable_set(:@env, Rage::Env.new("test"))
+    require "inertia/rspec"
+  end
+
+  after :context do
+    Rage.instance_variable_set(:@root, nil)
+    Rage.instance_variable_set(:@env, nil)
+  end
+
+  before do
+    allow(Inertia::Frontend).to receive(:version)
+    allow(Inertia::Frontend).to receive(:render_layout) do |data|
+      "<html>#{data}</html>"
+    end
+  end
+
+  describe "inertia response object" do
+    it "returns component name" do
+      get "/"
+      expect(inertia.component).to eq("Users/Index")
+    end
+
+    it "returns props with symbolized keys" do
+      get "/"
+      expect(inertia.props).to eq({
+        users: [{ id: 1, name: "John" }, { id: 2, name: "Jane" }],
+        meta: { total: 2 }
+      })
+    end
+
+    it "returns nested props with symbolized keys" do
+      get "/users/1"
+      expect(inertia.props[:user]).to eq({ id: 1, name: "John", email: "john@example.com" })
+    end
+
+    it "returns deferred props metadata" do
+      get "/users/1"
+      expect(inertia.deferred_props).to eq({ "default" => ["activity"] })
+    end
+
+    it "returns empty hash when no deferred props" do
+      get "/"
+      expect(inertia.deferred_props).to eq({})
+    end
+  end
+
+  describe "non-inertia responses" do
+    it "returns nil for plain text responses" do
+      get "/plain"
+      expect(inertia).to be_nil
+    end
+
+    it "returns nil for JSON responses" do
+      get "/json"
+      expect(inertia).to be_nil
+    end
+  end
+
+  describe "partial reloads" do
+    context "with :only option" do
+      it "requests only specified props" do
+        get "/users/1", inertia: { only: :user }
+
+        expect(inertia.props.keys).to eq([:user])
+        expect(inertia.props[:user]).to eq({ id: 1, name: "John", email: "john@example.com" })
+      end
+
+      it "requests multiple props with array" do
+        get "/users/1", inertia: { only: [:user, :permissions] }
+
+        expect(inertia.props.keys).to contain_exactly(:user, :permissions)
+        expect(inertia.props[:permissions]).to eq(%w[read write])
+      end
+
+      it "overwrites previous response" do
+        get "/users/1"
+        get "/users/1", inertia: { only: :user }
+
+        expect(inertia.props.keys).to eq([:user])
+      end
+    end
+
+    context "with :except option" do
+      it "excludes specified props" do
+        get "/", inertia: { except: :meta }
+
+        expect(inertia.props.keys).to eq([:users])
+      end
+
+      it "excludes multiple props with array" do
+        get "/users/1", inertia: { except: [:permissions, :activity] }
+
+        expect(inertia.props.keys).to eq([:user])
+      end
+    end
+
+    context "loading deferred props" do
+      it "loads deferred props when explicitly requested" do
+        get "/users/1"
+        expect(inertia.props.keys).to eq([:user])
+        expect(inertia.deferred_props).to eq({ "default" => ["activity"] })
+
+        get "/users/1", inertia: { only: :activity }
+        expect(inertia.props[:activity]).to eq(%w[login logout])
+      end
+    end
+
+    context "with invalid options" do
+      it "raises ArgumentError for unknown options" do
+        expect {
+          get "/users/1", inertia: { unknown: :option }
+        }.to raise_error(ArgumentError, /Unknown :inertia option/)
+      end
+    end
+  end
+end
